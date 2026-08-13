85a6986d9be14d5dc8642d768682525614b9005f
Soufiyan <soufiyanchaatani@gmail.com>
2026-08-13 13:07:11 +0100

    Update ColorSelector.vue

--- src/components/ColorSelector/ColorSelector.vue
+++ src/components/ColorSelector/ColorSelector.vue
@@ -13,35 +13,42 @@ const select = (color) => {
 
 <template>
   <div class="color-selector">
-    <h3 class="color-selector__title">Choose Color</h3>
-    <div class="color-selector__swatches" role="radiogroup" aria-label="Choose color">
-      <button
-        v-for="color in colors"
-        :key="color.sku"
-        type="button"
-        class="color-selector__swatch"
-        :class="{ 'color-selector__swatch--selected': modelValue?.sku === color.sku }"
-        :style="{ backgroundColor: color.code }"
-        role="radio"
-        :aria-checked="modelValue?.sku === color.sku"
-        :aria-label="color.name"
-        :title="color.name"
-        @click="select(color)"
-      >
-        <span v-if="modelValue?.sku === color.sku" class="color-selector__check" aria-hidden="true">
-          <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M5 12l5 5L19 7"
-              class="color-selector__check-icon"
-              stroke-width="2.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </span>
-      </button>
-    </div>
-    <p v-if="modelValue" class="color-selector__selected-label">Selected: {{ modelValue.name }}</p>
+    <template v-if="colors.length">
+      <h3 class="color-selector__title">Choose Color</h3>
+      <div class="color-selector__swatches" role="radiogroup" aria-label="Choose color">
+        <button
+          v-for="color in colors"
+          :key="color.sku"
+          type="button"
+          class="color-selector__swatch"
+          :class="{ 'color-selector__swatch--selected': modelValue?.sku === color.sku }"
+          :style="{ backgroundColor: color.code }"
+          role="radio"
+          :aria-checked="modelValue?.sku === color.sku"
+          :aria-label="color.name"
+          :title="color.name"
+          @click="select(color)"
+        >
+          <span v-if="modelValue?.sku === color.sku" class="color-selector__check" aria-hidden="true">
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M5 12l5 5L19 7"
+                class="color-selector__check-icon"
+                stroke-width="2.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </span>
+        </button>
+      </div>
+      <p v-if="modelValue" class="color-selector__selected-label">Selected: {{ modelValue.name }}</p>
+    </template>
+
+    <template v-else>
+      <h3 class="color-selector__title">Color</h3>
+      <p class="color-selector__empty-text" role="status">This product does not have color options.</p>
+    </template>
   </div>
 </template>
 
@@ -84,6 +91,11 @@ const select = (color) => {
 
 .color-selector__selected-label {
   margin-top: var(--space-1);
+  font-size: var(--fs-small);
+  color: var(--color-text-secondary);
+}
+
+.color-selector__empty-text {
   font-size: var(--fs-small);
   color: var(--color-text-secondary);
 }
